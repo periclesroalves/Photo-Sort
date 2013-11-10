f461ceb268da4fad7ae782d3cd1b628c83aa1502
System Administrator <root@Felipe-Guttierrezs-MacBook-Pro.local>
2013-11-09 19:22:13 -0600

    fb login button added

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php include "templates/header.php" ?>
 
-<?php echo "hello world"; ?>
+
+
+<div
+  class="fb-login-button"
+  data-show-faces="true"
+  data-width="200"
+  data-max-rows="1"
+  data-scope="publish_actions">
+</div>
 
 <?php include "templates/footer.php" ?>
 
--- templates/header.php
+++ templates/header.php
@@ -14,5 +14,29 @@
 </head>
 
 <body>
+<script type = "text/javascript">
+// Additional JS functions here
 
-<h1>THIS IS HTML</h1>
+	window.fbAsyncInit = function() {
+		FB.init({
+      		appId      : '643775449019374', // App ID
+      		status     : true,    // check login status
+      		cookie     : true,    // enable cookies to allow the
+                            // server to access the session
+      		xfbml      : true     // parse page for xfbml or html5
+                            // social plugins like login button below
+    	});
+
+	// Put additional init code here
+	};
+	//Load SDK 
+	(function(d, s, id){
+     	var js, fjs = d.getElementsByTagName(s)[0];
+     	if (d.getElementById(id)) {return;}
+     	js = d.createElement(s); js.id = id;
+     	js.src = "//connect.facebook.net/en_US/all.js";
+     	fjs.parentNode.insertBefore(js, fjs);
+   	}(document, 'script', 'facebook-jssdk'));
+</script>
+
+<h1>Photo Sort!</h1>
